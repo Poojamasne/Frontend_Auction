c43300e10f84098b14408c252922dde95aea4ace
Pooja Masne <143479102+Poojamasne@users.noreply.github.com>
2025-09-17 11:55:06 +0530

    Update MyCreatedAuctionView.tsx

--- src/components/Dashboard/MyCreatedAuctions/MyCreatedAuctionView.tsx
+++ src/components/Dashboard/MyCreatedAuctions/MyCreatedAuctionView.tsx
@@ -2056,7 +2056,7 @@ const MyCreatedAuctionView: React.FC = () => {
                   </div>
                   {(!preBid.status || preBid.status === "pending") && (
                     <div className="prebid-action-buttons">
-                      <button
+{/*                       <button
                         type="button"
                         onClick={() => handlePreBidAction(preBid.id, "approve")}
                         className="btn btn-primary btn-sm"
@@ -2064,7 +2064,7 @@ const MyCreatedAuctionView: React.FC = () => {
                       >
                         <CheckCircle className="w-4 h-4" />
                         Approve
-                      </button>
+                      </button> */}
                       <button
                         type="button"
                         onClick={() => handlePreBidAction(preBid.id, "reject")}
