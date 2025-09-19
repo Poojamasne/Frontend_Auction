8cc78493ed4e45e980c2b4caa9c84d68a9cd3e53
Developer <developer@sachins-MacBook-Air.local>
2025-09-19 18:56:28 +0530

    my changes

--- src/components/Admin/AdminHome/AdminHome.tsx
+++ src/components/Admin/AdminHome/AdminHome.tsx
@@ -393,7 +393,7 @@ const AdminHome: React.FC = () => {
           <div className="card-body">
             {/* <div className="grid grid-cols-3 md:grid-cols-3 gap-4"> */}
               {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"> */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-3 gap-4" id='quick-actions-grid'>
                 
 
 
